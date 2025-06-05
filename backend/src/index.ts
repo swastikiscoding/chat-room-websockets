37ac354e1d3634c55780999bca9871379c3eca89
Swastik <swastikiscoding@gmail.com>
2025-06-05 19:09:38 +0530

    fix: update route handling for production to match wildcard pattern

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ io.on("connection", (socket) => {
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
