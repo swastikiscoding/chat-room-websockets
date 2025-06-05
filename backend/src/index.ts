8f08a08e172345272c93f205bacaf56b7b6098cf
Swastik <swastikiscoding@gmail.com>
2025-06-05 19:03:57 +0530

    fix: update route handling for production environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ io.on("connection", (socket) => {
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
